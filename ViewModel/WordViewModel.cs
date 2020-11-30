99265d5574f916c539234da26baa3a9ac9f6e77f
psw221 <tpdnddlek@gmail.com>
2020-11-30 22:02:06 +0900

    초기 커밋.

---
+++ ViewModel/WordViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordBook.Classes;
+
+namespace WordBook.ViewModel
+{
+    public class WordViewModel : Notifier
+    {
+        public MyICommand MyCommand { get; set; }
+
+        public WordViewModel()
+        {
+            MyCommand = new MyICommand(OnAddList);
+        }
+
+        private string wordString;
+        public string WordString
+        {
+            get { return wordString; }
+            set
+            {
+                wordString = value;
+                OnPropertyChanged("WordString");
+            }
+        }
+
+        private string selectedWord;
+        public string SelectedWord
+        {
+            get { return selectedWord; }
+            set
+            {
+                selectedWord = value;
+                OnPropertyChanged("SelectedWord");
+            }
+        }
+
+        ObservableCollection<string> wordList;
+
+        public ObservableCollection<string> WordList
+        {
+            get
+            {
+                if (wordList == null)
+                    wordList = new ObservableCollection<string>();
+
+                return wordList;
+            }
+            set
+            {
+                wordList = value;
+            }
+        }
+
+        private void OnAddList()
+        {
+            if(!string.IsNullOrEmpty(WordString) && !WordList.Contains(WordString)) 
+                WordList.Add(WordString);
+        }
+
+        private bool CanAddList()
+        {
+            return SelectedWord != null;
+        }
+    }
+}
